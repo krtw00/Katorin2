34fc98244de8325e578667a33acd94966f1c38f3
krtw00 <krtw00@users.noreply.github.com>
2026-03-17 07:53:34 +0900

    fix: メタ分析ページにloading.tsx追加 (#21)
    全ルートでloading.tsxのカバレッジを100%に

---
+++ src/app/[locale]/(main)/series/[id]/meta/loading.tsx
@@ -0,0 +1,9 @@
+import { DetailPageSkeleton } from "@/components/ui/loading"
+
+export default function MetaAnalysisLoading() {
+  return (
+    <div className="container py-6">
+      <DetailPageSkeleton />
+    </div>
+  )
+}
