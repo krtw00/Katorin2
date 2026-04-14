05b4a878a9f68831e4643135478cdcf335174309
krtw00 <krtw00@users.noreply.github.com>
2026-04-14 14:24:10 +0900

    fix: lineup 参照ありの参加者削除を修正

--- apps/ledger/app/models/participant.rb
+++ apps/ledger/app/models/participant.rb
@@ -4,6 +4,7 @@ class Participant < ApplicationRecord
   belongs_to :league
   belongs_to :team
 
+  has_many :match_lineup_members, dependent: :destroy
   has_many :home_board_results, class_name: "BoardResult", foreign_key: :home_participant_id, dependent: :nullify
   has_many :away_board_results, class_name: "BoardResult", foreign_key: :away_participant_id, dependent: :nullify
 
