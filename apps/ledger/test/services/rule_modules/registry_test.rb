dd25b0d67777d2fe1c30755fc94c426cd0d78c22
krtw00 <krtw00@users.noreply.github.com>
2026-05-17 19:22:12 +0900

    KAT-7 refactor: Wmgp::LeagueModule skeleton + boot 時 Registry 登録

---
+++ apps/ledger/test/services/rule_modules/registry_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class RuleModules::RegistryTest < ActiveSupport::TestCase
+  test "wmgp module is registered with expected hooks" do
+    rule_module = RuleModules::Registry.fetch("wmgp")
+
+    assert_kind_of Wmgp::LeagueModule, rule_module
+    assert_equal "wmgp", rule_module.key
+    assert_equal Standings::Calculator, rule_module.rules.standings
+    assert_equal MatchExports::ResultCardRenderer, rule_module.renderers.match_result_card
+    assert_equal StandingsExports::TableRenderer, rule_module.renderers.standings_table
+  end
+
+  test "fetching unknown key raises KeyError" do
+    assert_raises(KeyError) do
+      RuleModules::Registry.fetch("unknown-league")
+    end
+  end
+
+  test "keys returns registered module keys" do
+    assert_includes RuleModules::Registry.keys, "wmgp"
+  end
+end
